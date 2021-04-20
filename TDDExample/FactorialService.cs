755abb264d9d0e9a46c8fe3d1e9703e6d0766a22
Jorge Yanes <jorgeyanesm@gmail.com>
2021-04-20 17:37:14 -0400

    Create FactorialService with method Calculate without code.

---
+++ TDDExample/FactorialService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDExample
+{
+    public class FactorialService
+    {
+        public int Calculate(int number) 
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
